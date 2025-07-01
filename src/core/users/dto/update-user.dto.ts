123142b1f8733ae8b69dc38a603c1f2063c27ab3
Chiran Jeewantha <67968706+Chikuu98@users.noreply.github.com>
2025-07-01 21:57:32 +0530

    implement user profile update (#26)
    * Add DTOs and update methods for user, member, and mentor details

---
+++ src/core/users/dto/update-user.dto.ts
@@ -0,0 +1,54 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+import { Gender } from '@/core/users/user.entity';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @Length(1, 100)
+  @ApiPropertyOptional({ 
+    example: 'John Smith',
+    description: 'User name'
+  })
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  @ApiPropertyOptional({ 
+    example: 'john.updated@example.com',
+    description: 'User email address'
+  })
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(Gender)
+  @ApiPropertyOptional({ 
+    enum: ['male', 'female', 'other'],
+    description: 'User gender'
+  })
+  gender?: Gender;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 100)
+  @ApiPropertyOptional({ 
+    example: 'Sri Lanka',
+    description: 'User country'
+  })
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 100)
+  @ApiPropertyOptional({ 
+    example: 'English',
+    description: 'User preferred language'
+  })
+  language?: string;
+}
